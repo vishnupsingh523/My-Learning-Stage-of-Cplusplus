5d1277149698106ca1f79a46aefda9684113f744
VISHWANATH PRATAP SINGH <49968961+vishnupsingh523@users.noreply.github.com>
2020-03-25 18:01:21 +0530

    file created

---
+++ Boy_OR_Girl.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    vector<char> values;
+    string enter;
+    cin>>enter;
+    for(int i=0;i<enter.size();i++)
+    values.push_back(enter[i]);
+    
+    sort(values.begin(),values.end());
+    
+    vector<char>::iterator ip;
+    
+    ip= unique(values.begin(),values.end());
+    
+    values.resize(distance(values.begin(),ip));
+    
+    if(values.size()%2==0)
+    cout<<"CHAT WITH HER!";
+    else
+    cout<<"IGNORE HIM!";
+    
+return 0;
+}
