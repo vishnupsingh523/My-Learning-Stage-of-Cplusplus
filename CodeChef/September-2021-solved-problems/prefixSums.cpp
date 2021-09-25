f722cf3bfa6d7fa63eb962efe1a64e0c36d73231
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2021-09-25 21:36:33 +0530

    september lunchtime 2021

---
+++ CodeChef/September-2021-solved-problems/prefixSums.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define vi vector<int>
+#define vf vector<float>
+#define vs vector<string>
+#define vll vector<long long>
+#define ll long long
+#define fl float
+#define db double
+#define st string
+
+#define w(x) int x;cin>>x;while(x--)
+#define endl "\n"
+ 
+#define sync ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    sync
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,a,b,c;
+        // cin>>n;
+        // if(n%4==0)
+        // cout<<"YES\n";
+        // else
+        // cout<<"NO\n";
+
+        ll l, r;
+        cin>>l>>r;
+        cout<<(r-l+1)/3<<endl;
+    }
+}
---
+++ CodeChef/September-2021-solved-problems/twoDishes.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define vi vector<int>
+#define vf vector<float>
+#define vs vector<string>
+#define vll vector<long long>
+#define ll long long
+#define fl float
+#define db double
+#define st string
+
+#define w(x) int x;cin>>x;while(x--)
+#define endl "\n"
+ 
+#define sync ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    sync
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a,b,c;
+        cin>>n>>a>>b>>c;
+        int ans = min(a,b);
+        b = b-ans;
+        ans += min(b,c);
+        
+        if(ans>=n)
+        cout<<"YES\n";
+        else
+        cout<<"NO\n";
+
+    }
+}
